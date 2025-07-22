1e77c00f2e24ba99d9e3dac38e29d7e9f7186875
nunnes <gustavonunnes@hotmail.com>
2025-07-21 23:14:04 -0300

    Atualização de código

--- src/Endereco.java
+++ src/Endereco.java
@@ -1,2 +1,7 @@
-public class Endereco {
+public record Endereco (String cep,
+                        String logradouro,
+                        String complemento,
+                        String localidade,
+                        String uf){
+    //RECORD, internamente já possui os getters|setters e ToString
 }
